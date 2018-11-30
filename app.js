9c329f3871d3881e4dabab92b169bc1518d781f0
Mattia <mattiasu96@live.it>
2018-11-30 23:09:12 +0100

    Working server with periodic sample data

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
-const http = require('http');
+var http = require("http");
+var url = require('url');
+var fs = require('fs');
+var io = require('socket.io');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var server = http.createServer(function(request, response){
+    var path = url.parse(request.url).pathname;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Ciao bellooooooo!\n');
+    switch(path){
+        case '/':
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write('hello world');
+            response.end();
+            break;
+        case '/socket.html':
+            fs.readFile(__dirname + path, function(error, data){
+                if (error){
+                    response.writeHead(404);
+                    response.write("opps this doesn't exist - 404");
+                    response.end();
+                }
+                else{
+                    response.writeHead(200, {"Content-Type": "text/html"});
+                    response.write(data, "utf8");
+                    response.end();
+                }
+            });
+            break;
+        default:
+            response.writeHead(404);
+            response.write("opps this doesn't exist - 404");
+            response.end();
+            break;
+    }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(8001);
+
+io.listen(server);
+
+var listener = io.listen(server);
+listener.sockets.on('connection', function(socket){
+   
+    setInterval(function(){socket.emit('message', {'message': 'Bella zio'})},1);
 });
